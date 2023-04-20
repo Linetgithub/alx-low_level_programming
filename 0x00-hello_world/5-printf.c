040857f1e23640a13c9c144aaa652f53edba932b
linetgithub <awinoojwang22@gmail.com>
2023-04-20 04:08:30 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * main - A program that prints a line using the printf function
- * Return:0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n")
