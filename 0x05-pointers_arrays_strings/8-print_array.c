30067d64d88aa11f0122dc2f283ce63681ab5466
linetgithub <awinoojwang22@gmail.com>
2023-05-02 12:44:02 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,19 +5,16 @@
  * print_array - prints n elements of an array of integers
  * @a: Array of integers
  * @n: Number of elements of the array to be printed
- * Return: void
  */
 void print_array(int *a, int n)
 {
-	int j;
+	int t;
 
-	for (j = 0; j < n; j++)
+	for (t = 0; t < n; t++)
 	{
-		printf("%d", a[j]);
-		if (j != (n - 1))
-		{
-			printf(",");
-		}
+		printf("%d", a[t]);
+		if (t != n - 1)
+			printf(", ");
 	}
 	printf("\n");
 }
