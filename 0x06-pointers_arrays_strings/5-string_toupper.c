b8f94b903d6e0afc0b3e87b12d565a1c481cbe4e
linetgithub <awinoojwang22@gmail.com>
2023-05-05 03:09:02 -0700

    task 6

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,8 +12,10 @@ char *string_touper(char *n)
 	i = 0;
 	while (n[i] != '\0')
 	{
-		if (n[i] >= 'a' && n[i] <= 'z')
+		if (n[i] >= 97 && n[i] <= 122)
+		{
 			n[i] = n[i] - 32;
+		}
 		i++;
 	}
 	return (n);
