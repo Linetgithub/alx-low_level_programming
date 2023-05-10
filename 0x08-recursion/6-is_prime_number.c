fb53a8fa1f3b6e6b18df1f06160b94538d881804
linetgithub <awinoojwang22@gmail.com>
2023-05-10 10:35:48 -0700

    task 7

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (actual_prime(n,n - 1));
+	return (actual_prime(n, n - 1));
 }
 
 /**
@@ -20,7 +20,7 @@ int is_prime_number(int n)
  * @n: the number to be checked
  * @i: the iteration times
  *
- * Return: 1 for prime or 0 composite
+ * Return: 1 for prime or 0 if not
  */
 int actual_prime(int n, int i)
 {
