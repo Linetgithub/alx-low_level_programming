2b5d56e180abb6409e85c421ab4c087b794357d2
linetgithub <awinoojwang22@gmail.com>
2023-05-12 05:28:44 -0700

    task 0

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: success 1
+ * on error, -1 is returned error is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
