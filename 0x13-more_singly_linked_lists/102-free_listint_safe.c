f2a79498ee372999c03037983f22a7a767de3cc1
Linetgithub <awinooojwang22@gmail.com>
2023-06-06 01:16:28 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,23 +9,28 @@
 size_t free_listint_safe(listint_t **h)
 {
 	size_t n = 0;
+	int diff;
 	listint_t i;
 
 	if (!h || !*h)
 		return (0);
 	while (*h)
 	{
-		i = (*h)->next;
-		free(*h);
-		*h = i;
-		n++;
-	}
-	else
-	{
-		free(*h);
-		*h = NULL;
-		n++;
-		break;
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			i = (*h)->next;
+			free(*h);
+			*h = i;
+			n++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			n++;
+			break;
+		}
 	}
 	*h = NULL;
 	return (n);
