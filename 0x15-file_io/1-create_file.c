807630a67e474f5beeab359b81a8eb236462c7cd
Linetgithub <awinooojwang22@gmail.com>
2023-06-12 06:28:58 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 
 /**
  * create_file - creates file
- * @text: pointer to string
+ * @text_content: pointer to string
  * @filename: pointer  name
  * Return: -1 or --1 for (NULL)
  */
@@ -11,15 +11,15 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
 
+	if (filename == NULL)
+		return (-1);
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
-
 	}
 
-	if (filename == NULL)
-		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
