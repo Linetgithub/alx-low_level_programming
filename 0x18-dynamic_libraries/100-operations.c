ef47011f476bc756b0564e8db982f3542c723889
Linetgithub <awinoojwang22@gmail.com>
2023-08-21 09:41:13 -0700

    task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+signed int add(int a, int b)
+{
+return (a + b);
+}
+signed int sub(int a, int b)
+{
+return (a - b);
+}
+signed int mul(int a, int b)
+{
+return (a * b);
+}
+signed int div(int a, int b)
+{
+return (a / b);
+}
+signed int mod(int a, int b)
+{
+return (a % b);
+}
